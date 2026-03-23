f76f29f51fe08bf48334ccebf4919faa49005b00
soronicepgf-rgb <soronice.pgf@gmail.com>
2026-03-23 19:17:45 -0400

    Add Lua obfuscation script
    Add obfuscated Lua script for execution

--- ytopkhijhirtjhiortnzuohnrt/rtjhgtiuhgrtiurthtrh/ruehtgiurtguirtuighrtz/ghrnzgirnzgirnzgihrzighr/gtzugzhgybhzzrgyerz/trhrtehioj,reojihneto/eiojg,rthijrthojrt,hoji,rtjioh,trjio,horj,hoji,hoijrt,ozihj,rjioz,hrjohirz:/GUIHRUIZGHZHGERYZHGYERZHGIYERHZGUERHZUIGERZIUGERZ/ZETGUIJZG9UUIZGZRUIGHERIZUHGIERUZGEZRUGUIERZ/GRTGHTUIZG?RUZGNRZNGRZNGUIRZNGIURZ/ergernugnnerhguyerngerygneru,gerjgurenug,jerhnguyerughberyghuer--l.lua
+++ ytopkhijhirtjhiortnzuohnrt/rtjhgtiuhgrtiurthtrh/ruehtgiurtguirtuighrtz/ghrnzgirnzgirnzgihrzighr/gtzugzhgybhzzrgyerz/trhrtehioj,reojihneto/eiojg,rthijrthojrt,hoji,rtjioh,trjio,horj,hoji,hoijrt,ozihj,rjioz,hrjohirz:/GUIHRUIZGHZHGERYZHGYERZHGIYERHZGUERHZUIGERZIUGERZ/ZETGUIJZG9UUIZGZRUIGHERIZUHGIERUZGEZRUGUIERZ/GRTGHTUIZG?RUZGNRZNGRZNGUIRZNGIURZ/ergernugnnerhguyerngerygneru,gerjgurenug,jerhnguyerughberyghuer--l.lua
@@ -1,1 +1,273 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/soronicepgf-rgb/S-curit-astrale/refs/heads/main/ytopkhijhirtjhiortnzuohnrt/rtjhgtiuhgrtiurthtrh/ruehtgiurtguirtuighrtz/ghrnzgirnzgirnzgihrzighr/gtzugzhgybhzzrgyerz/trhrtehioj%2Creojihneto/eiojg%2Crthijrthojrt%2Choji%2Crtjioh%2Ctrjio%2Chorj%2Choji%2Choijrt%2Cozihj%2Crjioz%2Chrjohirz%3A/GUIHRUIZGHZHGERYZHGYERZHGIYERHZGUERHZUIGERZIUGERZ/ZETGUIJZG9UUIZGZRUIGHERIZUHGIERUZGEZRUGUIERZ/GRTGHTUIZG%3FRUZGNRZNGRZNGUIRZNGIURZ/L.lua"))()
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
+
+]]--
+
+local StrToNumber = tonumber;
+local Byte = string.byte;
+local Char = string.char;
+local Sub = string.sub;
+local Subg = string.gsub;
+local Rep = string.rep;
+local Concat = table.concat;
+local Insert = table.insert;
+local LDExp = math.ldexp;
+local GetFEnv = getfenv or function()
+	return _ENV;
+end;
+local Setmetatable = setmetatable;
+local PCall = pcall;
+local Select = select;
+local Unpack = unpack or table.unpack;
+local ToNumber = tonumber;
+local function VMCall(ByteString, vmenv, ...)
+	local DIP = 1;
+	local repeatNext;
+	ByteString = Subg(Sub(ByteString, 5), "..", function(byte)
+		if (Byte(byte, 2) == 81) then
+			repeatNext = StrToNumber(Sub(byte, 1, 1));
+			return "";
+		else
+			local a = Char(StrToNumber(byte, 16));
+			if repeatNext then
+				local b = Rep(a, repeatNext);
+				repeatNext = nil;
+				return b;
+			else
+				return a;
+			end
+		end
+	end);
+	local function gBit(Bit, Start, End)
+		if End then
+			local Res = (Bit / (2 ^ (Start - 1))) % (2 ^ (((End - 1) - (Start - 1)) + 1));
+			return Res - (Res % 1);
+		else
+			local Plc = 2 ^ (Start - 1);
+			return (((Bit % (Plc + Plc)) >= Plc) and 1) or 0;
+		end
+	end
+	local function gBits8()
+		local a = Byte(ByteString, DIP, DIP);
+		DIP = DIP + 1;
+		return a;
+	end
+	local function gBits16()
+		local a, b = Byte(ByteString, DIP, DIP + 2);
+		DIP = DIP + 2;
+		return (b * 256) + a;
+	end
+	local function gBits32()
+		local a, b, c, d = Byte(ByteString, DIP, DIP + 3);
+		DIP = DIP + 4;
+		return (d * 16777216) + (c * 65536) + (b * 256) + a;
+	end
+	local function gFloat()
+		local Left = gBits32();
+		local Right = gBits32();
+		local IsNormal = 1;
+		local Mantissa = (gBit(Right, 1, 20) * (2 ^ 32)) + Left;
+		local Exponent = gBit(Right, 21, 31);
+		local Sign = ((gBit(Right, 32) == 1) and -1) or 1;
+		if (Exponent == 0) then
+			if (Mantissa == 0) then
+				return Sign * 0;
+			else
+				Exponent = 1;
+				IsNormal = 0;
+			end
+		elseif (Exponent == 2047) then
+			return ((Mantissa == 0) and (Sign * (1 / 0))) or (Sign * NaN);
+		end
+		return LDExp(Sign, Exponent - 1023) * (IsNormal + (Mantissa / (2 ^ 52)));
+	end
+	local function gString(Len)
+		local Str;
+		if not Len then
+			Len = gBits32();
+			if (Len == 0) then
+				return "";
+			end
+		end
+		Str = Sub(ByteString, DIP, (DIP + Len) - 1);
+		DIP = DIP + Len;
+		local FStr = {};
+		for Idx = 1, #Str do
+			FStr[Idx] = Char(Byte(Sub(Str, Idx, Idx)));
+		end
+		return Concat(FStr);
+	end
+	local gInt = gBits32;
+	local function _R(...)
+		return {...}, Select("#", ...);
+	end
+	local function Deserialize()
+		local Instrs = {};
+		local Functions = {};
+		local Lines = {};
+		local Chunk = {Instrs,Functions,nil,Lines};
+		local ConstCount = gBits32();
+		local Consts = {};
+		for Idx = 1, ConstCount do
+			local Type = gBits8();
+			local Cons;
+			if (Type == 1) then
+				Cons = gBits8() ~= 0;
+			elseif (Type == 2) then
+				Cons = gFloat();
+			elseif (Type == 3) then
+				Cons = gString();
+			end
+			Consts[Idx] = Cons;
+		end
+		Chunk[3] = gBits8();
+		for Idx = 1, gBits32() do
+			local Descriptor = gBits8();
+			if (gBit(Descriptor, 1, 1) == 0) then
+				local Type = gBit(Descriptor, 2, 3);
+				local Mask = gBit(Descriptor, 4, 6);
+				local Inst = {gBits16(),gBits16(),nil,nil};
+				if (Type == 0) then
+					Inst[3] = gBits16();
+					Inst[4] = gBits16();
+				elseif (Type == 1) then
+					Inst[3] = gBits32();
+				elseif (Type == 2) then
+					Inst[3] = gBits32() - (2 ^ 16);
+				elseif (Type == 3) then
+					Inst[3] = gBits32() - (2 ^ 16);
+					Inst[4] = gBits16();
+				end
+				if (gBit(Mask, 1, 1) == 1) then
+					Inst[2] = Consts[Inst[2]];
+				end
+				if (gBit(Mask, 2, 2) == 1) then
+					Inst[3] = Consts[Inst[3]];
+				end
+				if (gBit(Mask, 3, 3) == 1) then
+					Inst[4] = Consts[Inst[4]];
+				end
+				Instrs[Idx] = Inst;
+			end
+		end
+		for Idx = 1, gBits32() do
+			Functions[Idx - 1] = Deserialize();
+		end
+		return Chunk;
+	end
+	local function Wrap(Chunk, Upvalues, Env)
+		local Instr = Chunk[1];
+		local Proto = Chunk[2];
+		local Params = Chunk[3];
+		return function(...)
+			local Instr = Instr;
+			local Proto = Proto;
+			local Params = Params;
+			local _R = _R;
+			local VIP = 1;
+			local Top = -1;
+			local Vararg = {};
+			local Args = {...};
+			local PCount = Select("#", ...) - 1;
+			local Lupvals = {};
+			local Stk = {};
+			for Idx = 0, PCount do
+				if (Idx >= Params) then
+					Vararg[Idx - Params] = Args[Idx + 1];
+				else
+					Stk[Idx] = Args[Idx + 1];
+				end
+			end
+			local Varargsz = (PCount - Params) + 1;
+			local Inst;
+			local Enum;
+			while true do
+				Inst = Instr[VIP];
+				Enum = Inst[1];
+				if (Enum <= 3) then
+					if (Enum <= 1) then
+						if (Enum == 0) then
+							local A = Inst[2];
+							local Results, Limit = _R(Stk[A](Unpack(Stk, A + 1, Inst[3])));
+							Top = (Limit + A) - 1;
+							local Edx = 0;
+							for Idx = A, Top do
+								Edx = Edx + 1;
+								Stk[Idx] = Results[Edx];
+							end
+						else
+							Stk[Inst[2]] = Env[Inst[3]];
+						end
+					elseif (Enum == 2) then
+						local Edx;
+						local Results, Limit;
+						local B;
+						local A;
+						Stk[Inst[2]] = Env[Inst[3]];
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						Stk[Inst[2]] = Env[Inst[3]];
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						A = Inst[2];
+						B = Stk[Inst[3]];
+						Stk[A + 1] = B;
+						Stk[A] = B[Inst[4]];
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						Stk[Inst[2]] = Inst[3];
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						A = Inst[2];
+						Results, Limit = _R(Stk[A](Unpack(Stk, A + 1, Inst[3])));
+						Top = (Limit + A) - 1;
+						Edx = 0;
+						for Idx = A, Top do
+							Edx = Edx + 1;
+							Stk[Idx] = Results[Edx];
+						end
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						A = Inst[2];
+						Stk[A] = Stk[A](Unpack(Stk, A + 1, Top));
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						Stk[Inst[2]]();
+						VIP = VIP + 1;
+						Inst = Instr[VIP];
+						do
+							return;
+						end
+					else
+						local A = Inst[2];
+						Stk[A] = Stk[A](Unpack(Stk, A + 1, Top));
+					end
+				elseif (Enum <= 5) then
+					if (Enum > 4) then
+						Stk[Inst[2]] = Inst[3];
+					else
+						local A = Inst[2];
+						local B = Stk[Inst[3]];
+						Stk[A + 1] = B;
+						Stk[A] = B[Inst[4]];
+					end
+				elseif (Enum == 6) then
+					do
+						return;
+					end
+				else
+					Stk[Inst[2]]();
+				end
+				VIP = VIP + 1;
+			end
+		end;
+	end
+	return Wrap(Deserialize(), {}, vmenv)(...);
+end
+return VMCall("LOL!043Q00030A3Q006C6F6164737472696E6703043Q0067616D6503073Q00482Q747047657403C9012Q00682Q7470733A2Q2F7261772E67697468756275736572636F6E74656E742E636F6D2F736F726F6E6963657067662D7267622F532D63757269742D61737472616C652F726566732F68656164732F6D61696E2F79746F706B68696A686972746A68696F72746E7A756F686E72742F72746A68677469756867727469757274687472682F7275656874676975727467756972747569676872747A2F6768726E7A6769726E7A6769726E7A676968727A696768722F67747A75677A68677962682Q7A72677965727A2F74726872746568696F6A25324372656F6A69686E65746F2F65696F6A67253243727468696A7274686F6A7274253243686F6A6925324372746A696F6825324374726A696F253243686F726A253243686F6A69253243686F696A72742532436F7A69686A253243726A696F7A25324368726A6F6869727A2533412F475549485255495A47485A48474552595A48475945525A484749594552485A47554552485A55494745525A49554745525A2F5A45544755494A5A47392Q55495A475A52554947484552495A554847494552555A47455A525547554945525A2F47525447485455495A4725334652555A474E525A4E47525A4E475549525A4E474955525A2F4C2E6C756100083Q0012023Q00013Q00122Q000100023Q00202Q00010001000300122Q000300046Q000100039Q0000026Q000100016Q00017Q00", GetFEnv(), ...);
